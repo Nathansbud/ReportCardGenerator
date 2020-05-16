6a3e395e476d89b0cf6a3a442e6d7fe624d692f0
Zachary Amiton <zamiton@outlook.com>
2020-05-16 15:41:30 -0700

    trying to automate haiku as well...

--- veracross.py
+++ veracross.py
@@ -7,25 +7,37 @@ from selenium.webdriver.support.ui import WebDriverWait, Select
 import os
 import json
 
+import time
+
 vc_link = "https://accounts.veracross.eu/asb/portals/login"
+haiku_link = "https://asb.learning.powerschool.com/do/account/login"
 
 def make_browser(headless=True):
     options = webdriver.ChromeOptions()
     if headless: options.add_argument("headless")
-    browser = webdriver.Chrome(options=options)
-    browser.implicitly_wait(3)
-    return browser
+    b = webdriver.Chrome(options=options)
+    b.implicitly_wait(5)
+    return b
 
-def login(b, username, password):
-    b.find_element_by_id("username").send_keys(username)
-    b.find_element_by_id("password").send_keys(password)
+def vc_login(username, password, browser=None):
+    if not browser: browser = make_browser(False)
+
+    browser.find_element_by_id("username").send_keys(username)
+    browser.find_element_by_id("password").send_keys(password)
     try:
-        b.find_element_by_id("recaptcha").click()
+        browser.find_element_by_id("recaptcha").click()
     except NoSuchElementException:
-        b.find_element_by_id("login-button").click()
+        browser.find_element_by_id("login-button").click()
 
-def get_class_json(username, password, browser=make_browser(False)):
-    login(browser, username, password)
+def haiku_login(username, password, browser=None):
+    if not browser: browser = make_browser(False)
+    browser.find_element_by_id("parent_login").send_keys(username)
+    browser.find_element_by_id("password").send_keys(password)
+    browser.find_element_by_id("loginsubmit").click()
+
+def get_class_json(username, password, browser=None):
+    if not browser: browser = make_browser(False)
+    vc_login(username, password, browser=browser)
     try:
         if browser.find_element_by_class_name("warning"):
             return False
@@ -48,9 +60,10 @@ def get_class_json(username, password, browser=make_browser(False)):
         classes[code]["students"] = class_json
     return classes
 
-def automate_dropdowns(browser, username, password, link, l=None):
+def automate_dropdowns(username, password, link, browser=None, l=None):
+    if not browser: browser = make_browser(False)
     browser.get(link)
-    login(browser, username, password)
+    vc_login(username, password, browser)
     student_list = browser.find_elements_by_class_name("page-grade-form")[1]
     for student in student_list.find_elements_by_tag_name("li"):
         student.click()
@@ -64,3 +77,29 @@ def automate_dropdowns(browser, username, password, link, l=None):
             else:
                 try: dropdown.select_by_visible_text("ME")
                 except NoSuchElementException: dropdown.select_by_visible_text("S")
+
+
+def download_grades_from_haiku(username, password, link, browser=None):
+    if not browser: browser = make_browser(False)
+    browser.get(link)
+    haiku_login(username, password, browser)
+
+    if "accounts.google.com" in browser.current_url:
+        WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.ID, "identifierId"))).send_keys(username)
+        browser.find_element_by_id("identifierNext").click()
+        #by name since the id corresponds with enclosing div
+        WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.NAME, "password"))).send_keys(password)
+        browser.find_element_by_id("passwordNext").click()
+
+    browser.get("https://asb.learning.powerschool.com/amitons/math7-2019-20allsections/grades_sbg")
+    #not working?
+    print(WebDriverWait(browser, 15).until(EC.presence_of_element_located((By.TAG_NAME, "table"))))
+    tables = browser.find_elements_by_tag_name("table")
+    #table[0] seems to = student names, [2] = assignment names, [3] = grade data
+
+    print(tables)
+
+
+
+if __name__ == '__main__':
+    download_grades_from_haiku("", "", haiku_link)
